c63ba6a8e33dfd96c1239d19d7f851910970f9f7
Giorgio <giorgio.pignattari@gmail.it>
2021-06-08 17:00:43 +0200

    gestito il controller e creata rotta API

--- laravel-api/app/Http/Controllers/SportController.php
+++ laravel-api/app/Http/Controllers/SportController.php
@@ -14,72 +14,11 @@ class SportController extends Controller
      */
     public function index()
     {
-        //
-    }
+      $sports = Sport::all();
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Sport  $sport
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Sport $sport)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Sport  $sport
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Sport $sport)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Sport  $sport
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Sport $sport)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Sport  $sport
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Sport $sport)
-    {
-        //
+      return response()->json([
+        'data' => $sports,
+        'success' => true,
+      ]);
     }
 }
